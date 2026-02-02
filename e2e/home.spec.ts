66216e170cfbdc20986bf2619b4905c036692997
Rodrigo Silva De Oliveira <rodrigo.soliveira@meta.com.br>
2026-02-02 18:30:03 -0300

    Teste git actions3

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -4,6 +4,6 @@ test('acessar google', async ({ page }) => {
 
     await page.goto('http://google.com')
     // await expect(page).toHaveTitle('Gerencie suas tarefas com Mark L')//Titulo da pagina nop header
-    // await page.waitForTimeout(3000)
+     await page.waitForTimeout(3000)
 
 })
